048955bae9834fe57f1a4cf9451d87b120b44e86
James Russel C. Bautista <jrussclay@gmail.com>
2023-05-11 23:19:38 +0800

    Initial db setup

--- core/models.py
+++ core/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_published = models.BooleanField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="subjects"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_published = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name="courses"
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="courses"
+    )
+
+
+class Module(models.Model):
+    title = models.CharField(max_length=255)
+    duration = models.IntegerField()  # in minutes
+    is_published = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="modules")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="modules"
+    )
+
+
+class Content(models.Model):
+    content = models.TextField()
+    type = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    module = models.ForeignKey(
+        Module, on_delete=models.CASCADE, related_name="contents"
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="contents"
+    )
