70e67e83fa6e85aea99fc926c2e618c551f2a606
James Russel C. Bautista <jrussclay@gmail.com>
2023-05-14 09:34:31 +0800

    Refactor projects

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework_nested import routers
-from .views import SubjectViewSet
-
-
-router = routers.DefaultRouter()
-router.register("subjects", SubjectViewSet, basename="subjects")
-
-urlpatterns = router.urls
--- elearning/serializers.py
+++ elearning/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from authentication.serializers import UserSerializer
+from users.serializers import UserSerializer
 from .models import Subject
 
 
